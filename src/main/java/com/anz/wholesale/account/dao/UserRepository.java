6d7d7af2ec921491b00fc4cd91a3bc402669d1a6
turnse2 <turnse1@gmail.com>
2019-10-14 10:30:58 +1100

    Update UserRepository.java

--- src/main/java/com/anz/wholesale/account/dao/UserRepository.java
+++ src/main/java/com/anz/wholesale/account/dao/UserRepository.java
@@ -3,8 +3,6 @@ package com.anz.wholesale.account.dao;
 import com.anz.wholesale.account.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 
 /**A Repository for User entities implemented with Spring Data JPA.
  * @author Dennis Hu
