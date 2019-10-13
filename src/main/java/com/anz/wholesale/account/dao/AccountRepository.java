17d66ed09262e4d7200ffcfef5f322b27cd32c44
turnse2 <turnse1@gmail.com>
2019-10-14 10:29:56 +1100

    Update AccountRepository.java

--- src/main/java/com/anz/wholesale/account/dao/AccountRepository.java
+++ src/main/java/com/anz/wholesale/account/dao/AccountRepository.java
@@ -1,13 +1,7 @@
 package com.anz.wholesale.account.dao;
 
 import com.anz.wholesale.account.entities.Account;
-import com.anz.wholesale.account.entities.AccountTransaction;
-import com.anz.wholesale.account.entities.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
-
-import java.util.List;
 
 /**A Repository for Account entities implemented with Spring Data JPA.
  * @author Dennis Hu
